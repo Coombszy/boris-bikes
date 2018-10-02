2d806b15fd9f6d8d698a339819f41c64b82dfe6b
Liam <lccoombs@hotmail.co.uk>
2018-10-02 15:11:40 +0000

    removed a test from rspec as no longer applicable

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -5,10 +5,11 @@ describe DockingStation do
   it 'responds to Release_Bike' do
     expect(subject).to respond_to(:release_bike) 
   end
-  it 'returns a working bike' do
-    bike = subject.release_bike
-    expect(bike.working?).to be true 
-  end
+  #No longer valid
+  #it 'returns a working bike' do
+  #  bike = subject.release_bike
+  #  expect(bike.working?).to be true 
+  #end
   it 'responds to dock with an argument' do
     is_expected.to respond_to(:dock).with(1).argument
   end
