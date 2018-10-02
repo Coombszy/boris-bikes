724cf0a10ef95c1885e1387523b3ae0b531a8511
Liam <lccoombs@hotmail.co.uk>
2018-10-02 14:05:35 +0000

    Station successfully returning a bike that works

---
+++ getall.rb
@@ -0,0 +1,2 @@
+require './lib/DockingStation'
+require './lib/Bike'
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,9 +1,14 @@
 require 'DockingStation'
+require 'Bike'
 
 describe DockingStation do
-    it 'responds to Release_Bike' do
-       expect(subject).to respond_to(:release_bike) 
-    end
+  it 'responds to Release_Bike' do
+    expect(subject).to respond_to(:release_bike) 
+  end
+  it 'returns a working bike' do
+    bike = subject.release_bike
+    expect(bike.working?).to be true 
+  end
 end
 
     
